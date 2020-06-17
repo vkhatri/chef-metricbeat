bb347dfaf121a11ead8dfd76c26ff0e7ee99aafc
physhi <foobar22@hotmail.com>
2020-06-16 19:23:40 -0700

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ description 'Installs/Configures Elastic metricbeat'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.2.2'
 
-chef_version '~> 12'
+chef_version '>= 12' if respond_to?(:chef_version)
 
 source_url 'https://github.com/vkhatri/chef-metricbeat' if respond_to?(:source_url)
 issues_url 'https://github.com/vkhatri/chef-metricbeat/issues' if respond_to?(:issues_url)
