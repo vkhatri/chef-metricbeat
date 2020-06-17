d895dadb788c608aa68c563140fb1fbeaffd81fc
physhi <foobar22@hotmail.com>
2020-06-16 19:32:06 -0700

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'vir.khatri@gmail.com'
 license 'Apache-2.0'
 description 'Installs/Configures Elastic metricbeat'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.2.3'
+version '0.2.4'
 
 chef_version '>= 12' if respond_to?(:chef_version)
 
